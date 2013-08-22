a69566bc274ff07f1f272f75dfc378e1030a487e
stanmx <stanmx@gmail.com>
2013-08-22 14:41:54 -0700

    App created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Logodesigndeal::Application.config.secret_token = 'a2934813d29ddcc3f52931a080511104eaf020c6ccc18d1a587f3c16fcc225a7a2aa7308b0d79a241f7e869c8363fd14cfa10131526a5b283b81b55b74580c32'
